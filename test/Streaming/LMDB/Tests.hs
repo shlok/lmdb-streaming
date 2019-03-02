01e815dffd329a062cfa3701abb57c07ab1fcf27
Shlok Datye <shlok@quant.is>
2019-03-02 12:21:37 +0100

    Test insertion of duplicate keys, and refactor.

--- test/Streaming/LMDB/Tests.hs
+++ test/Streaming/LMDB/Tests.hs
@@ -7,7 +7,7 @@ module Streaming.LMDB.Tests (tests) where
 import Control.Concurrent.Async (asyncBound, wait)
 import Control.Monad (forM_)
 import Control.Monad.Trans.Resource (runResourceT)
-import Data.ByteString (pack)
+import Data.ByteString (ByteString, pack)
 import Data.List (sort)
 import qualified Data.Map.Strict as M (fromList, toList)
 import Database.LMDB.Raw (MDB_dbi', MDB_env, mdb_clear', mdb_put', mdb_txn_begin, mdb_txn_commit)
@@ -15,7 +15,7 @@ import Streaming.LMDB (readLMDB, writeLMDB)
 import Streaming.LMDB.Internal (marshalOut, noWriteFlags)
 import Streaming.Prelude (each, toList_)
 import qualified Streaming.Prelude as S (take)
-import Test.QuickCheck.Monadic (monadicIO, pick, run)
+import Test.QuickCheck.Monadic (PropertyM, monadicIO, pick, run)
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (arbitrary, testProperty)
 
@@ -31,9 +31,7 @@ tests res = [ testReadLMDB res, testWriteLMDB res ]
 testReadLMDB :: IO (MDB_env, MDB_dbi') -> TestTree
 testReadLMDB res = testProperty "readLMDB" . monadicIO $ do
     (env, dbi) <- run res
-    keyValuePairs <- removeDuplicateKeys . map (\(ws1, ws2) -> (pack ws1, pack ws2))
-                                         . filter (\(ws1, _) -> length ws1 > 0) -- LMDB does not allow empty keys.
-                                        <$> pick arbitrary
+    keyValuePairs <- arbitraryKeyValuePairs
     run $ (asyncBound $ do
         txn <- mdb_txn_begin env Nothing False
         mdb_clear' txn dbi
@@ -41,15 +39,10 @@ testReadLMDB res = testProperty "readLMDB" . monadicIO $ do
                                          marshalOut v $ \v' -> mdb_put' noWriteFlags txn dbi k' v' >> return ()
         mdb_txn_commit txn) >>= wait
     readPairsAll <- run . runResourceT . toList_ $ readLMDB env dbi
-    let allAsExpected = readPairsAll == sort keyValuePairs
+    let allAsExpected = readPairsAll == (sort . removeDuplicateKeys $ keyValuePairs)
     readPairsFirstFew <- run . runResourceT . toList_ . S.take 3 $ readLMDB env dbi
-    let firstFewAsExpected = readPairsFirstFew == (take 3 $ sort keyValuePairs)
+    let firstFewAsExpected = readPairsFirstFew == (take 3 . sort . removeDuplicateKeys $ keyValuePairs)
     return $ allAsExpected && firstFewAsExpected
-
-removeDuplicateKeys :: (Ord a) => [(a, b)] -> [(a, b)]
-removeDuplicateKeys = M.toList . M.fromList
-
---------------------------------------------------------------------------------
 
 -- | Clear the database, write key-value pairs to it using our streaming functionality,
 -- read them back using our streaming functionality (which 'testReadLMDB' already tests),
@@ -61,11 +54,19 @@ testWriteLMDB res = testProperty "writeLMDB" . monadicIO $ do
         txn <- mdb_txn_begin env Nothing False
         mdb_clear' txn dbi
         mdb_txn_commit txn) >>= wait
-    keyValuePairs <- removeDuplicateKeys . map (\(ws1, ws2) -> (pack ws1, pack ws2))
-                                         . filter (\(ws1, _) -> length ws1 > 0) -- LMDB does not allow empty keys.
-                                        <$> pick arbitrary
+    keyValuePairs <- arbitraryKeyValuePairs
     run . runResourceT . writeLMDB env dbi $ each keyValuePairs
     readPairs <- run . runResourceT . toList_ $ readLMDB env dbi
-    return $ readPairs == sort keyValuePairs -- We expect to get the keys back in sorted order.
+    return $ readPairs == (sort . removeDuplicateKeys $ keyValuePairs)
+
+arbitraryKeyValuePairs :: PropertyM IO [(ByteString, ByteString)]
+arbitraryKeyValuePairs =
+    map (\(ws1, ws2) -> (pack ws1, pack ws2))
+    . filter (\(ws1, _) -> length ws1 > 0) -- LMDB does not allow empty keys.
+   <$> pick arbitrary
+
+-- | Note that this function retains the last value for each key.
+removeDuplicateKeys :: (Ord a) => [(a, b)] -> [(a, b)]
+removeDuplicateKeys = M.toList . M.fromList
 
 --------------------------------------------------------------------------------
